3bbdc1bbb94f5898d2f419918c6cbdf28e934700
Parker <parkerbennion@gmail.com>
2021-01-30 13:08:56 -0700

    made prop spin
    challange complete

--- Plane Prg/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Plane Prg/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0,3f,-9f);
+    private Vector3 offset = new Vector3(0,3f,-11f);
 
     // Update is called once per frame
     void Update()
---
+++ Plane Prg/Assets/Challenge 1/Scripts/Propspin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propspin : MonoBehaviour
+{
+    public float propSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * propSpeed * Time.deltaTime);
+    }
+}
