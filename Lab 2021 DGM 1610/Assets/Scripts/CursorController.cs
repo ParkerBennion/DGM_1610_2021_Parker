47931bf2d39f927e6cdafd4e8fa7d8949f17d703
Parker <parkerbennion@gmail.com>
2021-02-16 21:58:27 -0700

    tried second method for getting cursor to work

--- Lab 2021 DGM 1610/Assets/Scripts/CursorController.cs
+++ Lab 2021 DGM 1610/Assets/Scripts/CursorController.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void update()
     {
-        
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
     }
 }
