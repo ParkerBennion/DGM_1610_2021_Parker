0a120de3626ba6afcfc566ff1144dd867cd22a76
Parker <parkerbennion@gmail.com>
2021-02-17 17:24:46 -0700

    Proj 3 changes

---
+++ Proj3/Assets/Scripts/Jump.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody FoxRb;
+    public float jumpforce;
+    public float gravForce;
+    public bool jumpAvailable;
+    void Start()
+    {
+        FoxRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravForce;
+        jumpAvailable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpAvailable)
+        {
+            FoxRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            jumpAvailable = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        jumpAvailable = true;
+    }
+}
