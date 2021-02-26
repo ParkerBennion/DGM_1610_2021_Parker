b423d8b50e9f14597d29407bd67b272b0c6412cd
Parker <parkerbennion@gmail.com>
2021-02-26 13:19:47 -0700

    worked on aiming
    gun will aim at cursos positoin in x,y axis but not the z. attpts were made to fix but solution has no been found. thinking of making it aim at a sprite fixed on z axis that follows curso on x,y axis.

--- Lab 2021 DGM 1610/Assets/Scripts/Aiming.cs
+++ Lab 2021 DGM 1610/Assets/Scripts/Aiming.cs
@@ -1,27 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Aiming : MonoBehaviour
 {
-    public Transform reticle;
+    //public Transform reticle;
 
-    public Vector3 orient;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //public Vector3 orient;
     void Update()
     {
-        orient.x =Input.GetAxis("Mouse X") * Time.deltaTime; //every frome the x cordnate on positoin will equal that of the mouse.
-        orient.y =Input.GetAxis("Mouse Y") * Time.deltaTime;
+        aim();
         
-        reticle.Rotate(Vector3.up * orient.x);
+        
+        
+        
+        //orient.x =Input.GetAxis("Mouse X") * Time.deltaTime; //every frome the x cordnate on positoin will equal that of the mouse.
+        //orient.y =Input.GetAxis("Mouse Y") * Time.deltaTime;
+        
+        //reticle.Rotate(Vector3.up * orient.x);
     }
+    
     //^ this didnt work. i appears to be more for a fps type world as it get a mouse rotation.
     // https://www.youtube.com/watch?v=_XdqA3xbP2A this tutorial is wehre im going to continue to find solution to scrips.
+    void aim()
+    {
+        Vector3 ready = Input.mousePosition;
+        ready = Camera.main.ScreenToWorldPoint(ready);
+
+        Vector2 direction = new Vector3(
+            ready.x - transform.position.x,
+            ready.y - transform.position.y,
+            ready.z - transform.position.z);
+        transform.forward = direction;
+        
+    }
+    
+
 }
---
+++ Lab 2021 DGM 1610/Assets/Scripts/follow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public Vector3 mosePosxy;
+    public float xBound, yBound, zBound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mosePosxy = Camera.main.ScreenToWorldPoint(mosePosxy);
+        mosePosxy.x = Input.mousePosition.x;
+        mosePosxy.y = Input.mousePosition.y;
+        mosePosxy.z = -4;
+        mosePosxy = Camera.main.ScreenToWorldPoint(mosePosxy);
+        transform.Translate(mosePosxy);
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.y < -yBound)
+        {
+            transform.position = new Vector3(-yBound, transform.position.x, transform.position.z);
+        }
+        if (transform.position.y > yBound)
+        {
+            transform.position = new Vector3(yBound, transform.position.x, transform.position.z);
+        }
+        
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(-zBound, transform.position.y, transform.position.x);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(zBound, transform.position.y, transform.position.x);
+        }
+        //^^ attempt to limit bouds to see what was happeneing as the object is flying really fast.
+        
+    }
+}
