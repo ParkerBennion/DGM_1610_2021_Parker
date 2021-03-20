4741a6d914d7ef8ca616c6dfb65c3a11edc3293b
Parker <parkerbennion@gmail.com>
2021-03-19 21:52:16 -0600

    added master kill box. fixed some geometry, added bullet trigger

---
+++ Lab 2021 DGM 1610/Assets/Scripts/Expire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expire : MonoBehaviour
+{
+    private float leftLimit = 15;
+    //private float rightLimit = 15;
+    //private float topLimit = 15;
+    private float botLimit = 15;
+    //private float froLimit = 15;
+    private float depthLimit = 15;
+    void Update()
+    {
+        if (transform.position.x < -leftLimit || transform.position.x > leftLimit ||
+            transform.position.y < -botLimit || transform.position.y > botLimit ||
+            transform.position.z < -depthLimit || transform.position.z > depthLimit)
+        {
+            Destroy(gameObject);
+        } 
+    }
+    //master kill box
+}
---
+++ Lab 2021 DGM 1610/Assets/Scripts/Kill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+        /*if (other.gameObject.CompareTag("bullet"))
+        {
+            Destroy(gameObject);
+        }*/
+    }
+}
