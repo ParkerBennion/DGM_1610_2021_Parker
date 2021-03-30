7b584e1b89f2f3cefab7fa6e8441b8d1129fef3e
Parker <parkerbennion@gmail.com>
2021-03-29 22:35:38 -0600

    3.3

--- Proj 4/Assets/Scripts/Controler.cs
+++ Proj 4/Assets/Scripts/Controler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class Controler : MonoBehaviour
     private Rigidbody playerRB;
     public float speed;
     private GameObject focalPoint;
+    private bool hasPowerUp;
     
     void Start()
     {
@@ -17,5 +19,14 @@ public class Controler : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed *Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
     }
 }
