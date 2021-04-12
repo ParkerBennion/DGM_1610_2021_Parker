5b1605a6693f9374a480ce1721189e16966e05c8
Parker <parkerbennion@gmail.com>
2021-04-12 17:09:47 -0600

    5.1

---
+++ Proj 5/Assets/Scripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    private float forwardInput = Input.GetAxis("Vertical");
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
